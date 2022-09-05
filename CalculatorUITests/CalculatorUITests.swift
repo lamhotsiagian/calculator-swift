20d9c487bc4cef1405036bf9a33016dcd16ec752
Lamhotjm <lamhot.id@gmail.com>
2022-09-05 15:10:33 -0700

    update the version

--- CalculatorUITests/CalculatorUITests.swift
+++ CalculatorUITests/CalculatorUITests.swift
@@ -43,7 +43,6 @@ class CalculatorUITests: XCTestCase {
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
-            // Put the code you want to measure the time of here.
         }
     }
     
